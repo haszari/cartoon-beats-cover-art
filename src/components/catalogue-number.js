58a09e30ae7e155009c2444c313a98f74d0f1b4d
haszari <haszari@cartoonbeats.com>
2023-06-17 09:20:03 +0200

    pad catalogue number with zeros

--- src/components/catalogue-number.js
+++ src/components/catalogue-number.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function CatalogueNumber( { index } ) {
   // we use letter o for our zeros in cat number
-  const cat = 'cbr' + index.padStart( 3, 'o' );
+  const cat = 'cbr' + index.padStart( 3, '0' );
   return (
     <div className='catalogue-number'>
       { cat }
